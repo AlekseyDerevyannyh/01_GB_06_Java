47f61d10adddad7e65ef8cdb4e8c776b41601dc2
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2022-12-12 10:46:04 +0300

    in progress HW 7(13) Java OOP

--- seminar/13/HW/Phonebook/model/contact_mapper_csv.py
+++ seminar/13/HW/Phonebook/model/contact_mapper_csv.py
@@ -1,13 +1,14 @@
 from model import contact
 from model import user
 
+
 class ContactMapperCsv:
     def map_to_str(self, _contact):
         if isinstance(_contact, contact.Contact):
             _user = _contact.get_user()
             if isinstance(_user, user.User):
                 return _contact.get_id() + ';' + _user.get_last_name() + ';' + \
-                    _user.set_first_name() + ';' + _user.get_patronymic() + ';' + \
+                    _user.get_first_name() + ';' + _user.get_patronymic() + ';' + \
                     _contact.get_phone()
             else:
                 return 'Input data error!'
@@ -17,4 +18,3 @@ class ContactMapperCsv:
     def map_from_str(self, line):
         lines = line.split(';')
         return contact.Contact(user.User(lines[2], lines[1], lines[3]), lines[4], lines[0])
-    
--- seminar/13/HW/Phonebook/model/repository.py
+++ seminar/13/HW/Phonebook/model/repository.py
@@ -1,2 +1,45 @@
+from model import contact_mapper_csv
+from model import file_operation
+from model import contact
+
+
 class Repository:
-    pass
+    def __init__(self, _file_operation):
+        self.__mapper = contact_mapper_csv.ContactMapperCsv()
+        self.__file_operation = _file_operation
+
+    def get_all_contact(self):
+        contacts = []
+        if isinstance(self.__file_operation, file_operation.FileOperation):
+            lines = self.__file_operation.read_all_lines()
+            for line in lines:
+                contacts.append(self.__mapper.map_from_str(line))
+        return contacts
+
+    def get_max_id(self, contacts):
+        id = 0
+        for _contact in contacts:
+            if isinstance(_contact, contact.Contact):
+                if int(_contact.get_id()) > id:
+                    id = int(_contact.get_id())
+        return id
+
+    def create_contact(self, _contact):
+        contacts = Repository.get_all_contact(self)
+        id = Repository.get_max_id(self, contacts)
+        id += 1
+        if isinstance(_contact, contact.Contact):
+            _contact.set_id(id)
+        Repository.save_contact(_contact, contacts)
+        return str(id)
+
+    def save_contact(self, _contact, contacts):
+        contacts.append(_contact)
+        Repository.save_contacts(contacts)
+
+    def save_contacts(self, contacts):
+        lines = []
+        for _contact in contacts:
+            lines.append(self.__mapper.map_to_str(_contact))
+        if isinstance(self.__file_operation, file_operation.FileOperation):
+            self.__file_operation.save_all_lines(lines)
